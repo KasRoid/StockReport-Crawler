53dc52fe7f3659d2327135191ec15cd14b3d60bd
KasRoid <KasRoid@gmail.com>
2022-12-08 18:37:42 +0900

    Report Page 기본 UI 셋업

--- StockReport/StockReport/ViewController.swift
+++ StockReport/StockReport/ViewController.swift
@@ -8,9 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        100
     }
 }
